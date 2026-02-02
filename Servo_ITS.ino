706274d9d8867667731319c3a828be7b0ef9fe00
Marco <marcoantoniolorusso@yahoo.it>
2026-02-02 11:30:24 +0100

    Aggiunta motore

--- Servo_ITS.ino
+++ Servo_ITS.ino
@@ -1,8 +1,9 @@
-
-
+// NON servono più librerie Servo!
 
 int ledpin = 4;       
 int pushbutton = 7;
+int motorPin = 9;  
+
 int val;
 int lastval = 0;
 int ledstate = LOW;    
@@ -11,6 +12,10 @@ void setup() {
   Serial.begin(9600);
   pinMode(ledpin, OUTPUT);
   pinMode(pushbutton, INPUT);
+  
+  pinMode(motorPin, OUTPUT);
+  
+  Serial.println("--- Sistema Motore DC Avviato ---");
 }
 
 void loop(){
@@ -18,13 +23,19 @@ void loop(){
 
   
   if (val == 1 && val != lastval){
+    
+    // Invertiamo lo stato
     ledstate = !ledstate;
-    digitalWrite(ledpin, ledstate); 
     
+
+    digitalWrite(ledpin, ledstate);  
+    digitalWrite(motorPin, ledstate); 
+    Serial.println("ON -> Motore ACCESO");
     
-    delay(50); 
+ 
+    
+    delay(100); 
   }
   
   lastval = val; 
 }
-
